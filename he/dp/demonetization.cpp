926500a2a3fbe1a26903b2377ef773051691e7eb
Ajith Kumar <ajith.kumar.b@hgst.com>
2017-12-18 18:08:30 +0900

    Minor changes to demonetization recursive

--- he/dp/demonetization.cpp
+++ he/dp/demonetization.cpp
@@ -13,66 +13,65 @@ class Solver {
     int amount;
     int ndenoms;
     vector<int> denoms = vector<int>(max_coins);
-    vector<int> mincache = vector<int>(max_amount + 1, invalid_min_cnt);
-    int findmincoins(int amount, int upper_bound);
+    vector<int> mincache = vector<int>(max_amount + 1);
+    int findmincoins(int amount);
     public:
-        Solver(istream &in);
-        int findmincoins();
+        void init(istream &in);
+        inline int findmincoins() {
+            return findmincoins(amount);
+        }
 };
 
-Solver::Solver(istream &in) {
+void Solver::init(istream &in) {
     int denom;
 
     in >> amount >> ndenoms;
 
-    mincache.at(0) = 0;
+    mincache[0] = 0;
+    for (auto a=1;a<=amount;a++) {
+        mincache[a]=invalid_min_cnt;
+    }
     for (auto c=0;c<ndenoms;c++) {
         in >> denom;
-        mincache.at(denom) = 1;
+        mincache[denom] = 1;
         denoms[c] = denom;
     }
-    sort(denoms.begin(), denoms.begin() + ndenoms, std::greater<int>());
 }
 
-int Solver::findmincoins() {
-    return findmincoins(amount, invalid_min_cnt);
-}
-
-int Solver::findmincoins(int amount, int upper_bound) {
+int Solver::findmincoins(int amount) {
     int min = invalid_min_cnt, tmin;
 
-    //cout << "fmc(" << amount << ") = ";
-    
     /* Find if the min count is already calculated earlier and in cache */
-    if (mincache.at(amount) != invalid_min_cnt) {
-        return mincache.at(amount);
+    if (mincache[amount] != invalid_min_cnt) {
+        return mincache[amount];
     }
 
     /* findmincoins(amount) = minforall_d(1 + findmincoins(amount - d))*/
     for(auto d=0;d<ndenoms;d++) {
-        auto denom = denoms.at(d);
-        if (denom <= amount && (upper_bound != invalid_min_cnt || upper_bound > 0)) {
-            tmin = findmincoins(amount - denom, (upper_bound == invalid_min_cnt ? invalid_min_cnt : upper_bound - 1));
+        auto denom = denoms[d];
+        if (denom <= amount) {
+            tmin = findmincoins(amount - denom);
             if (tmin != invalid_min_cnt) {
-                mincache.at(amount - denom) = tmin;
+                mincache[amount - denom] = tmin;
                 if ((1 + tmin) < min) {
                     min = 1 + tmin;
-                    upper_bound = min;
                 }
             }
         }
     }
-    //cout << min << "\n";
     return min;
 }
 
 int main()
 {
     int ntc, min;
+    Solver s;
+
     cin >> ntc;
 
     for (auto i=0;i<ntc;i++) {
-        min = Solver(cin).findmincoins();
+        s.init(cin);
+        min = s.findmincoins();
         if (min != invalid_min_cnt) cout << min << "\n";
         else cout << "No solution" << "\n";
     }
